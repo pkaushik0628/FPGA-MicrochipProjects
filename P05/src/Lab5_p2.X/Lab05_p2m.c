70dde5cc7b1bcdb2f920af6d8f324d1644e0fbec
Padmanabh Kaushik <pkaushik0628@gmail.com>
2025-01-24 15:45:35 -0500

    Update Lab05_p2m.c

--- P05/src/Lab5_p2.X/Lab05_p2m.c
+++ P05/src/Lab5_p2.X/Lab05_p2m.c
@@ -1,36 +1,17 @@
 /* 
  * File:   Lab05_p2m.c
- * Author: pazerao
- *
  * Created on October 4, 2022, 9:00 AM
  */
 
-
-
-/*
- * 
- */
 #include <xc.h>
-
 #include "ece212.h"
 
-/*
- * 
- */
 int main() {
     
     ece212_lafbot_setup();
     while(1){
-        move();
-        
-        
-        
+        move();      
     }
-    
-    
-    
-    
-
     return (EXIT_SUCCESS);
 }
 
